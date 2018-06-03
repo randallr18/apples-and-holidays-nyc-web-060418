643b005db026dc3254d54a9d86c80fc933ef47e3
Risher Randall <randallr18@mail.wlu.edu>
2018-06-03 19:36:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,7 +98,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season, values|
+  holiday_hash.collect do |season, values|
     values.collect do |holiday, supplies|
       if supplies.index('BBQ') != nil
         binding.pry
